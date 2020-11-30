4fe3c6f372eaf4e810793a5f5d8f9febeb45ef5d
gethiskg <atai2497@gmail.com>
2020-11-30 11:05:11 +0600

    firstOne

---
+++ TaskApp/app/src/main/java/com/example/taskapp/ui/form/FormFragment.java
@@ -0,0 +1,40 @@
+package com.example.taskapp.ui.form;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.taskapp.R;
+
+
+
+public class FormFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_form, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.btnSave).setOnClickListener(new View.OnClickListener() {
+            private static final String CLICKED = "ololo";
+
+            @Override
+            public void onClick(View v) {
+                Log.d(CLICKED, "onClick: button clicked");
+            }
+        });
+    }
+}
